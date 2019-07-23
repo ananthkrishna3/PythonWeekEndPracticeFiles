1533541c69b7a747581adc2f92e75b4b148c134c
ananthkrishna3 <ananthkrishna2112@gmail.com>
2019-07-23 22:36:49 +0530

    Add files via upload
    Python Day1-Weekend. Strings and its functionality

---
+++ Day1-Weekend.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 21 19:11:09 2019
+
+@author: AnanthKrishna
+"""
+
+print("hello world!")
+##Introduction to variables
+x=10
+y=5
+print(x+y)
+
+message= "Hello to python World!"
+print(message)
+
+name="Tony stark"
+print(name)
+print(name.title())
+print(name.upper())
+print(name.lower())
+##formatting string
+First_name = "Tony"
+Last_name = "stark"
+
+full_name=f"{First_name} {Last_name}"
+
+print(full_name)
+
+
+# adding whitespace to strings with tabs and newline
+
+print("python ")
+
+print("\tpython")
+print("\npython")
+
+#reducing or stripping the whitespaces
+fav_lang = "python "
+print(fav_lang.rstrip())
+
+
